0f6f4117fc14e932040361119f41cd726da68ea2
王传营 <1420209259@qq.com>
2020-03-29 20:47:15 +0800

    删除

--- src/main/java/com/zhxx/service/szxt/service/DemoService.java
+++
@@ -1,8 +0,0 @@
-package com.zhxx.service.szxt.service;
-
-import com.zhxx.service.szxt.beans.HttpResponseEntity;
-
-public interface DemoService {
-    //查询下拉框选项
-    HttpResponseEntity demoselService();
-}
--- src/main/java/com/zhxx/service/szxt/utils/NewEquipmentNo.java
+++
@@ -1,9 +0,0 @@
-package com.zhxx.service.szxt.utils;/**
- * Created by Administrator on 2018/12/17.
- */
-
-import com.zhxx.service.szxt.entity.DimensionEntity;
-import com.zhxx.service.szxt.mapper.DimensionEntityMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import java.text.DecimalFormat;
